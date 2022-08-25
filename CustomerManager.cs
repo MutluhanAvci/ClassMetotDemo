36eb587d2a7c943ac620e80d9766de39edcfcead
Mutluhan Avci <65674782+MutluhanAvci@users.noreply.github.com>
2022-08-25 04:08:12 +0300

    Add project files.

---
+++ CustomerManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class CustomerManager
+    {
+        public void CreateCustomer(Customer customer)
+        {
+            Console.WriteLine("New customer record created for "+customer.Name+" "+customer.Surname);
+        }
+        public void ShowAllCustomers(params Customer [] customers)
+        {
+            foreach (Customer item in customers)
+            {
+                Console.WriteLine(item.Name+" "+item.Surname);
+            }
+        }
+        public void DeleteCustomerRecord(Customer customer)
+        {
+            Console.WriteLine("Customer record "+customer.CustomerId+ " has been deleted succesfully");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.Name = "Mutluhan Yılmaz";
+            customer.Surname = "Avcı";
+            customer.CustomerId = 123;
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.CreateCustomer(customer);
+            customerManager.ShowAllCustomers(customer);
+            customerManager.DeleteCustomerRecord(customer);
+        }
+    }
+}
